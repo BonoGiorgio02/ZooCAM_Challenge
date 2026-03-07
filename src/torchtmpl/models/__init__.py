1be9fd6dd522460f76c7a61a82a9455d8f37fc7f
Giorgio Bono <giorgio.bono@student-cs.fr>
2026-03-07 18:41:37 +0100

    adding EfficientNet model to the init file

--- src/torchtmpl/models/__init__.py
+++ src/torchtmpl/models/__init__.py
@@ -8,7 +8,7 @@ from .base_models import *
 from .cnn_models import *
 from .resnet_model import *
 from .ResNet_models import *
-
+from .EfficientNet_models import *
 
 def build_model(cfg, input_size, num_classes):
     return eval(f"{cfg['class']}(cfg, input_size, num_classes)")
